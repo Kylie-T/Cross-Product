3eb73025229e1fa81cd6ad0dd5b853808e4812ac
Kylie-T <kyliebellet@gmail.com>
2025-09-09 21:36:56 -0500

    Update CrossProductTest.java to ask if run again

--- CrossProductTest.java
+++ CrossProductTest.java
@@ -1,14 +1,39 @@
-class CrossProductTest {
+import java.util.Scanner;
+
+class DecimalTest {
     public static void main(String[] args) {
-        CrossProduct crpr = new CrossProduct();
-        while (crpr.mayRun) {
 
-            crpr.askForVector();
-            crpr.calculate();
+        boolean mayRun = true;
 
-            System.out.printf("<%f, %f, %f>%n", crpr.products[0], crpr.products[1], crpr.products[2]);
+        while (mayRun) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number");
+            double num = scanner.nextDouble();
 
-            crpr.running();
+            System.out.println("Enter the base");
+            int base = scanner.nextInt();
+
+            Decimal conversion = new Decimal(num, base);
+
+            conversion.conIntBack();
+            for(int i = 0; i < conversion.conInt().size(); i++) {
+                System.out.print(conversion.conInt().get(i));
+            }
+            System.out.print(".");
+
+            for (int i = 0; i < conversion.conDec().size(); i++) {
+                System.out.print(conversion.conDec().get(i));
+            }
+
+            if (conversion.conDec().size() > 10) {
+                System.out.println("...");
+            }
+            System.out.println("Another? (type n or y)");
+            Scanner scannerAns = new Scanner(System.in);
+            String answer = scannerAns.nextLine();
+            if (answer.charAt(0) == 'n') {
+                mayRun = false;
+            }
         }
     }
 }
